e7e0dc3e1475cc6f3ff8123f5c9757c8ef208b97
Vinay Kashyap <vinaykashyap57@gmail.com>
2019-05-03 10:16:15 +0530

    Entry point update

--- EntryPoint.js
+++ EntryPoint.js
@@ -1,52 +1,55 @@
-var io= require("socket.io")({ transports: ['websocket'], });
-var SocketSettings= require("./API/ServerSettings/SocketSettings");
-var ServerEvents= require("./API/ServerEvents");
-var Player= require("./API/Data Models/Player");
+var io = require("socket.io")({ transports: ['websocket'], });
+var SocketSettings = require("./API/ServerSettings/SocketSettings");
+var ServerEvents = require("./API/ServerEvents");
+var Player = require("./API/Data Models/Player");
 
-var playerList={};
-var socketList={};
+var playerList = {};
+var socketList = {};
 io.attach(SocketSettings.socketPort);
 
 io.on('connection', function (socket) {
-    var player= new Player();
-    var playerID= player.playerID;
-    playerList[playerID]= player;
-    socketList[playerID]= socket;
+    var player = new Player();
+    var playerID = player.playerID;
+    playerList[playerID] = player;
+    socketList[playerID] = socket;
 
-    console.log('A user connected');  
-    console.log('player pos '+player.position);
+    console.log('A user connected');
+    console.log('player pos ' + player.position);
 
-    socket.emit(ServerEvents.ON_USER_CONNECTED,{playerID: playerID, playerPosition: player.position});//PlayerID for myself
-    
-    var opponentData= {
-        opponentID: playerID,
-        opponentPosition : player.position
-    }
-    var keys=Object.keys(playerList);
-    for(var i=0;i<keys.length;i++){
-        var data={
-            opponentID: keys[i],
-            opponentPosition : playerList[keys[i]].position
+    socket.emit(ServerEvents.ON_USER_CONNECTED, { playerID: playerID, playerPosition: player.position });//PlayerID for myself
+
+    var keys = Object.keys(playerList);
+    for (var i = 0; i < keys.length; i++) {
+        if(keys[i]==playerID){
+            continue;
         }
-        socket.emit(ServerEvents.ON_OPPONENT_CONNECTED,data);
+        var data = {
+            opponentID: keys[i],
+            opponentPosition: playerList[keys[i]].position
+        }
+        socket.emit(ServerEvents.ON_OPPONENT_CONNECTED, data);
+    }
+    var opponentData = {
+        opponentID: playerID,
+        opponentPosition: player.position
     }
     socket.broadcast.emit(ServerEvents.ON_OPPONENT_CONNECTED, opponentData);
 
-    socket.on(ServerEvents.MOVE_FORWARD,function(data){
+    socket.on(ServerEvents.MOVE_FORWARD, function (data) {
         //console.log("move forward called");
-       var newpos= player.MoveForward();
-        socket.emit(ServerEvents.ON_MOVE_FORWARD,{playerID:player.playerID,newPosition : newpos});
-        socket.broadcast.emit(ServerEvents.ON_MOVE_BACKWARD,{playerID:player.playerID,newPosition : newpos});
+        var newpos = player.MoveForward();
+        socket.emit(ServerEvents.ON_MOVE_FORWARD, { playerID: player.playerID, newPosition: newpos });
+        socket.broadcast.emit(ServerEvents.ON_MOVE_BACKWARD, { playerID: player.playerID, newPosition: newpos });
     });
-    socket.on(ServerEvents.MOVE_BACKWARD,function(data){
+    socket.on(ServerEvents.MOVE_BACKWARD, function (data) {
         //console.log("move backward called");
-       var newpos= player.MoveBackward();
-        socket.emit(ServerEvents.ON_MOVE_BACKWARD,{playerID:player.playerID,newPosition : newpos});       
-        socket.broadcast.emit(ServerEvents.ON_MOVE_BACKWARD,{playerID:player.playerID,newPosition : newpos});       
+        var newpos = player.MoveBackward();
+        socket.emit(ServerEvents.ON_MOVE_BACKWARD, { playerID: player.playerID, newPosition: newpos });
+        socket.broadcast.emit(ServerEvents.ON_MOVE_BACKWARD, { playerID: player.playerID, newPosition: newpos });
     });
-    socket.on("disconnect",function(data){
+    socket.on("disconnect", function (data) {
         console.log("disconnected player");
-        socket.emit(ServerEvents.ON_USER_DISCONNECTED,{playerID:player.playerID});
+        socket.emit(ServerEvents.ON_USER_DISCONNECTED, { playerID: player.playerID });
         delete playerList[playerID];
         delete socketList[playerID];
     });
